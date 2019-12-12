6a49c83ee34c161f2e0636ed18abe5dbec6b5668
workingPanda <marijan.000@gmail.com>
2019-12-12 20:30:51 +0100

    Update statistika_bacanja_kockice.js

--- statistika_bacanja_kockice.js
+++ statistika_bacanja_kockice.js
@@ -6,20 +6,20 @@ while (numTim < y) {
     totalDice.push(currentDice);
     numTim++
 }
-// let x = 1;
-// while (x < 7) {
-//     console.log(`${x+0}: ` + totalDice.filter(item => item === x).length / y*600 + '%');
-//     x++
-// }
 let x = 1;
 while (x < 7) {
-    var kocka = totalDice.filter(item => item === x).length / y*600;
-    var redni = x+0;
-    console.log(redni);
-    console.log(kocka);
-    document.getElementById("kocke" + redni).value = kocka;
-    document.getElementById("kocke" + redni).name = redni;
-    document.getElementById("kockice" + redni).innerHTML = redni +': ' +kocka+'%';
-    document.getElementById("kocak").innerHTML = 'Broj bacanja: ' +numTim+' puta';
+    console.log(`${x+0}: ` + totalDice.filter(item => item === x).length / y*600 + '%');
     x++
 }
+// let x = 1;
+// while (x < 7) {
+//     var kocka = totalDice.filter(item => item === x).length / y*600;
+//     var redni = x+0;
+//     console.log(redni);
+//     console.log(kocka);
+//     document.getElementById("kocke" + redni).value = kocka;
+//     document.getElementById("kocke" + redni).name = redni;
+//     document.getElementById("kockice" + redni).innerHTML = redni +': ' +kocka+'%';
+//     document.getElementById("kocak").innerHTML = 'Broj bacanja: ' +numTim+' puta';
+//     x++
+// }
