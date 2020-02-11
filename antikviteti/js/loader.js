1dea5612502abbd28697306e1ea0bb4e7a28764b
workingPanda <58808954+workingPanda@users.noreply.github.com>
2020-02-11 14:42:48 +0100

    Update loader.js

--- antikviteti/js/loader.js
+++ antikviteti/js/loader.js
@@ -1,17 +1,3 @@
-// reply_click('index')
-
-// function reply_click(clicked_id) {
-//    // console.log(clicked_id);
-//    var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://workingpanda.github.io/antikviteti/3body' + clicked_id + '.html', true);
-// xhr.onreadystatechange = function () {
-//    if (this.readyState !== 4) return;
-//    if (this.status !== 200) return; // or whatever error handling you want
-//    document.getElementById('c').innerHTML = this.responseText;
-// };
-// xhr.send();
-// }
-
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://workingpanda.github.io/antikviteti/nav.html', true);
