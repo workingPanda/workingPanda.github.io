bc38667bb5edc7173133f1ef4a88d0d69784d1cf
workingPanda <marijan.000@gmail.com>
2020-02-11 21:09:03 +0100

    Delete loaderGIT.js

--- antikviteti/img/js/loaderGIT.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-   var includes = $('[data-include]');
-   jQuery.each(includes, function(){
-     var file = 'https://workingpanda.github.io/antikviteti/' + $(this).data('include') + '.html';
-     $(this).load(file);
-   });
- });
