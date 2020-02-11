ded8bb2b1400e6bb844866272ed279591a3ec6ca
workingPanda <58808954+workingPanda@users.noreply.github.com>
2020-02-11 16:09:08 +0100

    Add files via upload

--- antikviteti/js/loader.js
+++ antikviteti/js/loader.js
@@ -1,10 +1,23 @@
+// reply_click('index')
+
+// function reply_click(clicked_id) {
+//    // console.log(clicked_id);
+//    var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://workingpanda.github.io/antikviteti/3body' + clicked_id + '.html', true);
+// xhr.onreadystatechange = function () {
+//    if (this.readyState !== 4) return;
+//    if (this.status !== 200) return; // or whatever error handling you want
+//    document.getElementById('c').innerHTML = this.responseText;
+// };
+// xhr.send();
+// }
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://workingpanda.github.io/antikviteti/nav.html', true);
 xhr.onreadystatechange = function () {
    if (this.readyState !== 4) return;
    if (this.status !== 200) return; // or whatever error handling you want
-   document.getElementById('n').innerHTML = this.responseText;
+   document.getElementById('navbar').innerHTML = this.responseText;
 };
 xhr.send();
 
@@ -13,7 +26,7 @@ xhr.open('GET', 'https://workingpanda.github.io/antikviteti/foot.html', true);
 xhr.onreadystatechange = function () {
    if (this.readyState !== 4) return;
    if (this.status !== 200) return; // or whatever error handling you want
-   document.getElementById('f').innerHTML = this.responseText;
+   document.getElementById('footer').innerHTML = this.responseText;
 };
 xhr.send();
 
